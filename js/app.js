57e8fc0be54e47d01ad07b8bc9205017b47d5fc8
mdnajmul <mdnajmulovi@gmail.com>
2021-08-20 15:55:00 +0600

    complete pin matcher

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 function getPin(){
+    //generate random 4 digit number
     const pinNumber = Math.round(Math.random()*10000);
+    //make number into string
     const pinString = pinNumber + '';
     if(pinString.length == 4){
         return pinNumber;
@@ -11,10 +13,36 @@ function getPin(){
 
 function generatePin(){
     const pin = getPin();
-    const pinInputField = document.getElementById('generate-pin-field');
-    pinInputField.value = pin;
+    document.getElementById('generate-pin-field').value = pin;
 }
 
 document.getElementById('keypad-numbers').addEventListener('click', function(event){
-    console.log(event.target);
+    const typeInputField = document.getElementById('type-number-field');
+    const typeNumber = event.target.innerText;
+    if(isNaN(typeNumber)){
+        if(typeNumber == 'C'){
+            typeInputField.value = '';
+            document.getElementById('success-message').style.display = 'none';
+            document.getElementById('error-message').style.display = 'none';
+        }
+    }
+    else{
+        typeInputField.value = typeInputField.value + typeNumber;
+    }
 });
+
+
+function verifyPin(){
+    const typeInputField = document.getElementById('type-number-field');
+    const generatePinInputField = document.getElementById('generate-pin-field');
+    const successMessageField = document.getElementById('success-message');
+    const errorMessageField = document.getElementById('error-message');
+    if(typeInputField.value == generatePinInputField.value){
+        successMessageField.style.display = 'block';
+        errorMessageField.style.display = 'none';
+    }
+    else{
+        successMessageField.style.display = 'none';
+        errorMessageField.style.display = 'block';
+    }
+}
